82fd249c76084e0b223fe0ff55bb15bcc1508fbc
Yoojin Lee <80568037+yoojinlee-hub@users.noreply.github.com>
2022-08-30 18:48:49 +0900

    Add files via upload

--- src/main/java/com/firstpractice/sample1.kt
+++ src/main/java/com/firstpractice/sample1.kt
@@ -1,9 +1,18 @@
 package com.firstpractice
 
 fun main(){
-    helloWorld()
 
-    add(4,9)
+    //3. String Template
+    val name = "yoojin"
+    val lastName = "lee"
+    println("my name is ${name + lastName}")
+
+    println("this is 2\$a") //그냥 $표시하려면 \
+
+    /**
+     *
+     * *
+     * */
 }
 
 //1. 함수
@@ -13,4 +22,17 @@ fun helloWorld() : Unit { //return 형식이 없음 = void
 
 fun add(a : Int, b : Int) : Int {
     return a+b
+}
+
+//2. val vs var
+//val = value
+
+fun hi(){
+    val a : Int = 10 //상수
+    var b : Int = 9 //변수
+
+    val c = 10 //자동으로 안다
+    var d = 100 //자동으로 안다
+
+    var name : String = "yoojin"
 }
