ab9669b07f36c3d0f3e2d9766fb2fcf4f2b1b21d
Yoojin Lee <80568037+yoojinlee-hub@users.noreply.github.com>
2022-09-13 13:44:24 +0900

    make splash

--- lotto/app/src/main/java/com/example/lotto/SplashActivity.kt
+++ lotto/app/src/main/java/com/example/lotto/SplashActivity.kt
@@ -2,10 +2,25 @@ package com.example.lotto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
+
+    private val SPLASH_TIME_OUT: Long = 3000  //3 sec
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+
+        //https://velog.io/@sinbee0402/AndroidKotlin-Lottie-Splash
+        Handler().postDelayed({  //3초(SPLASH_TIME_OUT)가 지나면 이 메서드가 실행됨
+            //main activity 시작
+            startActivity(Intent(this, MainActivity::class.java))
+            //액티비티 닫기
+            finish()
+        }, SPLASH_TIME_OUT)
     }
+
 }
