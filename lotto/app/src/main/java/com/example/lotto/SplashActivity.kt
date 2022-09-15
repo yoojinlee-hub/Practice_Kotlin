afe567489683e8bcbbff4d80647cd76a29a1e57f
Yoojin Lee <80568037+yoojinlee-hub@users.noreply.github.com>
2022-09-15 19:54:44 +0900

    Update SplashActivity.kt

--- lotto/app/src/main/java/com/example/lotto/SplashActivity.kt
+++ lotto/app/src/main/java/com/example/lotto/SplashActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
 import android.os.Handler
+import android.os.Looper
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
 
@@ -13,9 +15,23 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        animationView.setOnClickListener{
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
 
+        val handler = Handler(Looper.getMainLooper())
+        val runnable = Runnable{
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        handler.postDelayed(runnable,3000)
+
+        
         //https://velog.io/@sinbee0402/AndroidKotlin-Lottie-Splash
-        Handler().postDelayed({  //3초(SPLASH_TIME_OUT)가 지나면 이 메서드가 실행됨
+        Handler(Looper.getMainLooper()).postDelayed({  //3초(SPLASH_TIME_OUT)가 지나면 이 메서드가 실행됨
             //main activity 시작
             startActivity(Intent(this, MainActivity::class.java))
             //액티비티 닫기
