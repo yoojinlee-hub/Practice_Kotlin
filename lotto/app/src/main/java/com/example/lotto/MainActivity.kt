77b749ad648f7390cf2c9bc9db61678af7acb5b8
Yoojin Lee <80568037+yoojinlee-hub@users.noreply.github.com>
2022-09-16 19:46:31 +0900

    finish the lottery app

--- lotto/app/src/main/java/com/example/lotto/MainActivity.kt
+++ lotto/app/src/main/java/com/example/lotto/MainActivity.kt
@@ -2,13 +2,40 @@ package com.example.lotto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.os.CountDownTimer
+import android.os.Handler
+import android.os.Looper
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val lotteryNumbers = arrayListOf(number1, number2, number3, number4, number5, number6)
+
+        val countDownTimer = object:CountDownTimer(3000,10){
+            override fun onFinish() {
+            }
+
+            override fun onTick(p0: Long) {
+                lotteryNumbers.forEach{
+                    val randomNumber = (Math.random() * 45 + 1).toInt()
+                    it.text = "$randomNumber"
+                }
+            }
+        }
+
         lotteryButton.setOnClickListener{
+            if (lotteryButton.isAnimating){
+                lotteryButton.cancelAnimation()
+                countDownTimer.cancel()
+            }else{
+                lotteryButton.playAnimation()
+                countDownTimer.start()
+            }
 
         }
     }
