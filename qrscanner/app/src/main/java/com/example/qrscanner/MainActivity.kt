4725206f9ef8898cb762ac2f4ed4a70aa4c343f5
Yoojin Lee <80568037+yoojinlee-hub@users.noreply.github.com>
2022-09-19 18:40:39 +0900

    update 0919

--- qrscanner/app/src/main/java/com/example/qrscanner/MainActivity.kt
+++ qrscanner/app/src/main/java/com/example/qrscanner/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.qrscanner
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.google.zxing.integration.android.IntentIntegrator
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +15,28 @@ class MainActivity : AppCompatActivity() {
 
     fun startBarcodeReader(view: View){
         IntentIntegrator(this).initiateScan()
+    }
+
+    fun startBarcodeReaderCustom(view: View){
+        val integrator = IntentIntegrator(this)
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+        integrator.setPrompt("QR코드를 스캔하여 주세요")
+        integrator.setCameraId(0)
+        integrator.setBeepEnabled(true)
+        integrator.setBarcodeImageEnabled(true)
+        integrator.initiateScan()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if(result!=null){
+            if(result.contents != null){
+                Toast.makeText(this,"scanned : ${result.contents} format : ${result.formatName}", Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(this,"Cancelled",Toast.LENGTH_LONG).show();
+            }
+        }else{
+            super.onActivityResult(requestCode, resultCode, data)
+        }
     }
 }
