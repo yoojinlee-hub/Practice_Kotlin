31bfb05e3d59b9e1a63717eafe47db0e29d819f1
yoojinlee <80568037+yoojinlee-hub@users.noreply.github.com>
2022-09-30 20:47:55 +0900

    add few codes

--- KotlinAppLogin/app/src/main/java/com/example/kotlinapplogin/MainActivity.kt
+++ KotlinAppLogin/app/src/main/java/com/example/kotlinapplogin/MainActivity.kt
@@ -1,17 +1,24 @@
 package com.example.kotlinapplogin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_secound.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         bt_login.setOnClickListener(){
             //Intent 화면전환 key->value / id->id
+            val intent = Intent(this.SecoundActivity::class.java)//화면전환
 
+            //id가 다음화면에 보여야함
+            intent.putExtra("WMI",ed_id.text.toString())
+            startActivity(intent)
         }
     }
 }
