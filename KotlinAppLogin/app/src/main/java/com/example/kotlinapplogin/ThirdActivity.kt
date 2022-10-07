9ba52d0fed96a80f1bed78a1fc723d210d13b70c
Yoojin Lee <80568037+yoojinlee-hub@users.noreply.github.com>
2022-10-07 18:51:44 +0900

    edit web settings

--- KotlinAppLogin/app/src/main/java/com/example/kotlinapplogin/ThirdActivity.kt
+++ KotlinAppLogin/app/src/main/java/com/example/kotlinapplogin/ThirdActivity.kt
@@ -2,10 +2,14 @@ package com.example.kotlinapplogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_third.*
 
 class ThirdActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third)
+
+        web_V.settings.javaScriptEnabled = true
+
     }
 }
