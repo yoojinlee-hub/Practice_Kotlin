37c7e57af3c38d33c86abfa91d62ad6f9498f5e7
yoojinlee <80568037+yoojinlee-hub@users.noreply.github.com>
2022-10-09 15:02:51 +0900

    final commit in appLogin

--- KotlinAppLogin/app/src/main/java/com/example/kotlinapplogin/SecoundActivity.kt
+++ KotlinAppLogin/app/src/main/java/com/example/kotlinapplogin/SecoundActivity.kt
@@ -1,8 +1,11 @@
 package com.example.kotlinapplogin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.activity_secound.*
+import kotlinx.android.synthetic.main.activity_third.*
 
 class SecoundActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +15,12 @@ class SecoundActivity : AppCompatActivity() {
         if(intent.hasExtra("WMI")){
             tv_id2.text = intent.getStringExtra("WMI") //value값이 넘어옴
         }
+        val intent = Intent(this,ThirdActivity::class.java)//화면전환
+
+        web_V.settings.javaScriptEnabled = true
+        web_V.webViewClient = WebViewClient()
+        //web_V.webChromeClient = webChromeClient()
+        //url address
+        web_V.loadUrl("https://eclass.dongduk.ac.kr/ilos/main/main_form.acl")
     }
 }
--- KotlinAppLogin/app/src/main/java/com/example/kotlinapplogin/ThirdActivity.kt
+++ KotlinAppLogin/app/src/main/java/com/example/kotlinapplogin/ThirdActivity.kt
@@ -1,15 +1,22 @@
 package com.example.kotlinapplogin
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebViewClient
+import androidx.annotation.RequiresApi
 import kotlinx.android.synthetic.main.activity_third.*
 
 class ThirdActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third)
 
         web_V.settings.javaScriptEnabled = true
-
+        web_V.webViewClient = WebViewClient()
+        //web_V.webChromeClient = webChromeClient()
+        //url address
+        web_V.loadUrl("https://eclass.dongduk.ac.kr/ilos/main/main_form.acl")
     }
 }
